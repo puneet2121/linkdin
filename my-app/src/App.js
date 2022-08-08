ea8a1980577e356d677ffd9d1b6dbc768e8fec57
puneet2121 <bedipuneet21@gmail.com>
2022-08-08 14:19:05 -0600

    Set up files for linedIn clone

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Header';
+function App() {
+  return (
+    <div className="App">
+     
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      {/* Side Bar */ }
+      {/* Feed */}
+      {/* Widgets */}
+      {}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header'>
+      <h1>This is the header component </h1>
+      <div className='header_left'>
+
+      <div className='header_search'>
+        {/* SearchIcon */}
+        
+        </div>
+
+      </div>
+      <div className='header_right'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
