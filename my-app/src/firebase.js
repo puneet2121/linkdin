ce3a9ef5c9c14140d6a95864383341de8476573e
puneet2121 <bedipuneet21@gmail.com>
2022-08-18 00:50:36 -0600

    add env file and firebase

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/compat/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwUCWlxhzuCBvoEhio-7Q7LjCJhqb4T-U",
-  authDomain: "linkedin-project-c8e5b.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
   projectId: "linkedin-project-c8e5b",
   storageBucket: "linkedin-project-c8e5b.appspot.com",
-  messagingSenderId: "3298414977",
-  appId: "1:3298414977:web:51cbe30f545591b4639880",
-  measurementId: "G-CNRWP6V797"
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID ,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
